e1ef95815cfa34a90d1b46905203b82aa403ea6b
Sherwin <sherwinlukes@gmail.com>
2020-01-03 13:08:50 +0500

    initial commit (login screen)

---
+++ ATM_Dashboard1/ATM_Login.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ATM_Dashboard1
+{
+    /// <summary>
+    /// Interaction logic for ATM_Login.xaml
+    /// </summary>
+    public partial class ATM_Login : Window
+    {
+        public ATM_Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show($"Welocme: {txtname.Text} with password {txtpass.Password}");
+            MainWindow dashboard = new MainWindow();
+            dashboard.Show();
+        }
+    }
+}
