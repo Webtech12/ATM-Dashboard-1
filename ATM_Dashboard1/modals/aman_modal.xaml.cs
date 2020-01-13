709a52dfa0c7a2a86e749f77af1af2ac4362f194
Sherwin <sherwinlukes@gmail.com>
2020-01-13 11:54:04 +0500

    d-apo commit

--- ATM_Dashboard1/modals/aman_modal.xaml.cs
+++ ATM_Dashboard1/modals/aman_modal.xaml.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.Data;
 using System.Globalization;
 using System.Linq;
-using System.Text.RegularExpressions;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
